6fb0c8ab0c7ab3bb9e7d11db3eb250fbed091812
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-04 12:15:32 +0900

    演習3:フォーマットした状態で合計金額を表示

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,9 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/exam3")
 public class Exam03Controller {
 
+	@Autowired
+	private ServletContext application;
+
 	@RequestMapping("")
 	public String index() {
 		return "exam03";
 	}
-	
+
+	@RequestMapping("/result")
+	public String result(int item1, int item2, int item3) {
+		double tax = 1.1;
+		int sum = item1 + item2 + item3;
+		double taxSum = (item1 + item2 + item3) * tax;
+
+		application.setAttribute("sum", sum);
+		application.setAttribute("taxSum", taxSum);
+
+		return "exam03-result";
+
+	}
 }
