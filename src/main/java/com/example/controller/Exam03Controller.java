3c91d644e4a2dc59224e9d7e6131b24ab911dd34
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-04 12:00:21 +0900

    演習3の入力フォーム作成

---
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam3")
+public class Exam03Controller {
+
+	@RequestMapping("")
+	public String index() {
+		return "exam03";
+	}
+	
+}
