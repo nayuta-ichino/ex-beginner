f19ee4aa311f12f85611a3ed09b699a452408230
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-04 13:44:22 +0900

    演習4 UserFormクラス作成

--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,5 +1,40 @@
 package com.example.form;
 
 public class UserForm {
+	// フィールド変数
+	private String name;
+	private Integer age;
+	private String comment;
+
+	// setter,getter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "UserForm [name=" + name + ", age=" + age + ", comment=" + comment + "]";
+	}
 
 }
