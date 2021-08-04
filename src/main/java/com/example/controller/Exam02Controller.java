55302f76b160c388a2e76fe97b444b527ab2d390
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-04 11:44:01 +0900

    Controllerと計算結果画面１を作成

---
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam02")
+public class Exam02Controller {
+
+	@Autowired
+	private HttpSession session;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam02";
+	}
+
+	@RequestMapping("/result")
+	public String result(int num1, int num2) {
+		int answer = num1 + num2;
+
+		session.setAttribute("num1", num1);
+		session.setAttribute("num2", num2);
+		session.setAttribute("answer", answer);
+
+		return "exam02-result";
+	}
+	
+}
