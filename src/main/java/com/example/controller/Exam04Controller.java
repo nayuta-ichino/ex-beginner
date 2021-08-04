c4fd6b64d1c10ed266c6f0f027f27e7ffb1890cb
市野　那由他 <67714705+nayuta-ichino@users.noreply.github.com>
2021-08-04 14:05:47 +0900

    演習4 入力結果画面の表示完成

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,14 +1,33 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/exam04")
 public class Exam04Controller {
 
+	// フォームクラスを使えるように準備
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+
 	@RequestMapping("")
 	public String index() {
 		return "exam04";
+	}
+
+	@RequestMapping("/result")
+	public String result(UserForm userForm, Model model) {
+		model.addAttribute("name", userForm.getName());
+		model.addAttribute("age", userForm.getAge());
+		model.addAttribute("comment", userForm.getComment());
+		
+		return "exam04-result";
 	}
 }
